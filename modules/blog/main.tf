8fcf77bbc55d40c659ba178cb4902dea37709b60
Keith Wong <kmr_Wong@hotmail.com>
2023-11-07 15:29:41 -0600

    Moving to East 2

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -21,7 +21,7 @@ module "blog_vpc" {
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
-  azs             = ["us-west-1a","us-west-1b","us-west-1c"]
+  azs             = ["us-east-2a","us-east-2b","us-east-2c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
 
